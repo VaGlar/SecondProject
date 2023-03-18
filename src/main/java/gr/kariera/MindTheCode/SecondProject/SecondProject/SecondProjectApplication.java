3cd7ec683eeec825303377e71528091d079f5e65
VasilisGl <vasilisglaros@gmail.com>
2023-03-18 13:30:44 +0200

    Initial commit

---
+++ src/main/java/gr/kariera/MindTheCode/SecondProject/SecondProject/SecondProjectApplication.java
@@ -0,0 +1,13 @@
+package gr.kariera.MindTheCode.SecondProject.SecondProject;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SecondProjectApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SecondProjectApplication.class, args);
+	}
+
+}
